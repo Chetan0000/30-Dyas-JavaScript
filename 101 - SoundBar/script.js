aba275a0ad3340058b168a2095d65626324c7bdf
Chetan0000 <84121932+Chetan0000@users.noreply.github.com>
2023-09-07 19:02:40 +0530

    Add files via upload

---
+++ 101 - SoundBar/script.js
@@ -0,0 +1,24 @@
+function playSound(e){
+    keys.forEach( key => key.addEventListener('transitionend',removeTT));
+    function removeTT(e){
+        this.classList.remove('playing');
+    }
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key =  document.querySelector(`div[data-key="${e.keyCode}"]`)
+
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform') return;
+
+    this.classList.remove('playing');
+}
+
+window.addEventListener('keydown',playSound);
+const keys = document.querySelectorAll('.key');
+console.log(keys);
+keys.forEach( key => key.addEventListener('transitionend', removeTransition));
